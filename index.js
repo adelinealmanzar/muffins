e29493e217f22cd67f85a732c85cb53ec6fd8c25
Adeline Almanzar <almanzar.adeline@gmail.com>
2021-12-21 11:04:19 -0500

    feat: add javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement('h2');
+h2.textContent = "Hello! Welcome to my first website"
+
+document.querySelector('body').appendChild(h2);
+
+const p = document.createElement('p');
+p.textContent = "I love to bake! My banana nut muffins are pretty legendary.";
+
+document.queryCommandIndeterm('body').appendChild(p);
